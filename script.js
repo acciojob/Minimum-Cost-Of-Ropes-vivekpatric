5bea20e61ba55f2595626f5bfa7c10296513fd2d
Vivek Patric R <96299357+vivekpatric@users.noreply.github.com>
2023-05-31 14:42:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
-function calculateMinCost() {
+function calculateMinCost(arr) {
   //your code here
-  
+  let pq = [];
+   
+        // Adding items to the pQueue
+        for (let i = 0; i < n; i++) {
+            pq.push(arr[i]);
+        }   
+           
+        pq.sort(function(a,b){return a-b;});
+         
+        // Initialize result
+        let res = 0;
+   
+        // While size of priority queue
+        // is more than 1
+        while (pq.length > 1) {
+            // Extract shortest two ropes from pq
+            let first = pq.shift();
+            let second = pq.shift();
+   
+            // Connect the ropes: update result
+            // and insert the new rope to pq
+            res += first + second;
+            pq.push(first + second);
+            pq.sort(function(a,b){return a-b;});
+        }
+   
+        return res;
   
   
 }  
